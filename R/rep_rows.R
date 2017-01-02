cd1fbbf85da54fd7f7afdab8aaac19fe8872b2e0
jrnold <jeffrey.arnold@gmail.com>
2017-01-02 09:29:11 -0800

    bugfix in rep_rows
    - Wrong initial argument to rep
    - times argument cannot be NA

--- R/rep_rows.R
+++ R/rep_rows.R
@@ -4,7 +4,13 @@
 #' @param times See \code{\link{rep}}
 #' @param each See \code{\link{rep}}
 #' @return A data frame with replicated rows
-rep_rows <- function(.data, times = NA, each = NA) {
-  idx <- rep(rep_rows, each = each, times = times)
+#' @export
+#' @examples
+#' require("tibble")
+#' x <- tibble(a = 1:2, b = 3:4)
+#' rep_rows(x, 2)
+#' rep_rows(x, each = 2)
+rep_rows <- function(.data, times = 1, each = NA) {
+  idx <- rep(seq_len(nrow(.data)), each = each, times = times)
   .data[idx, ]
 }
