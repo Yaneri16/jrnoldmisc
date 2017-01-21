0ef260d69f13ea49f36a412e7c1e4628a725d4b8
jrnold <jeffrey.arnold@gmail.com>
2017-01-21 15:54:50 -0800

    misc

--- R/select_col.R
+++ R/select_col.R
@@ -5,22 +5,36 @@
 #' just a wrapper for `[[`, but is more
 #' descriptive and pipe friendly.
 #'
+#' If the \code{nm} argument is used, then this
+#' can be seen as the inverse of \code{\link[tibble]{enframe}}.
+#'
 #' @param col In \code{select_col} an unquoted
 #'   column name. In \code{select_col_}, the
 #'   column name as a string.
+#' @param nm If non-\code{NULL}, names to apply to the vector.
+#'   In \code{select_col} an unquoted column name.
+#'   In \code{select_col_}, the column name as a string.
 #' @param .data A data frame
 #' @export
 #' @examples
 #' select_col(iris, Species)
 #' select_col_(iris, "Species")
-select_col <- function(.data, col) {
+select_col <- function(.data, col, nm = NULL) {
+  if (!is.null(nm)) {
+    nm <- col_name(substitute(nm))
+  }
   col <- col_name(substitute(col))
-  select_col_(.data, col)
+  select_col_(.data, col, nm = nm)
 }
 
 #' @rdname select_col
 #' @export
-select_col_ <- function(.data, col) {
+select_col_ <- function(.data, col, nm = NULL) {
   assert_that(is.string(col))
-  .data[[col]]
+  assert_that(is.null(nm) | is.string(nm))
+  x <- .data[[col]]
+  if (!is.null(nm)) {
+    names(x) <- .data[[nm]]
+  }
+  x
 }
